9f9131763b6e054c4ad58f8ca04e9e7c1061c1eb
Thiagoads457 <thiagoads457@gmail.com>
2024-11-29 22:07:00 -0300

    29112024

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Refeicoes;
 
 class DashboardController extends Controller
 {
-    //
+    Public function gerarGrafico()
+    {
+        // Obter os dados de refeicoes, com a média de quantidade por 'informacoes_id'
+        $data = Refeicoes::select('informacoes_id', DB::raw('AVG(quantidade) as quantidade_media'))
+                ->groupBy('informacoes_id')
+                ->get();
+
+    
+        // Inicializando os arrays para categorias e valores médios
+        $informacoes = [];
+        $quantidades_media = [];
+    
+        // Verificando se os dados estão sendo processados corretamente
+        foreach ($data as $linha) {
+            $informacoes[] = $linha->informacoes->nome ?? 'N/A';  // Verifica se o nome da informacao existe
+            $quantidades_media[] = $linha->quantidade_media;
+        }
+    
+        // Exibir os dados para verificar
+        
+    
+        // Retornar a view com os dados processados
+        return view('dashboard', compact('informacoes', 'quantidades_media'));
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,38 @@
 <x-app-layout>
 
-    <h5 class="mt-3">Bem vindo ao Sistema de Controle de Calorias!</h5>
+    <h5 class="mt-3">Bem-vindo ao sistema de controle de estoque de refeições!</h5>
 
+    <!-- Carregando a biblioteca Google Charts -->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-
+        // Converte os dados para o formato adequado ao Google Charts
         var data = google.visualization.arrayToDataTable([
-          ['Refeição', 'Calorias 100gr'],
-          ['Arroz Branco Cozido',     130],
-          ['Feijão Carioca Cozido',      77],
-          ['Peito de Frango Grelhado',  165],
-          ['Batata Frita', 312],
-          ['Farofa',    350]
+          ['Informações', 'Quantidade Média'],
+          @foreach($informacoes as $index => $informacao)
+            ["{{$informacao}}", {{$quantidades_media[$index]}}],
+          @endforeach
         ]);
 
+        // Definindo as opções para o gráfico
         var options = {
-          title: '% de Calorias em uma Refeição (100gr)'
+          title: 'Média de Quantidade(gr) de Refeições por Paciente'
         };
 
+        // Criando o gráfico de pizza
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
         chart.draw(data, options);
       }
     </script>
 
+    <!-- Área onde o gráfico será exibido -->
     <div class="d-flex justify-content-center">
         <div id="piechart" style="width: 900px; height: 500px;"></div>
     </div>
+
 </x-app-layout>
+
+
